af9d1c34fa13eb3ecb5f446ba2f4e6301154ff14
JOHN-167 <91348778+JOHN-167@users.noreply.github.com>
2022-04-05 18:39:10 -0400

    Add files via upload

--- TicTacToes.java
+++ TicTacToes.java
@@ -2,25 +2,22 @@
  * Author: Quoc Duy Nguyen
  * NetID: qnguy12
  * Assignment: Project 3
- * 
- * Note: Add in checking score
  */
 
-import java.awt.Graphics; import java.awt.Color; import java.awt.Component;
-import java.awt.Font; import java.awt.Dimension;
+import java.awt.Graphics; import java.awt.Font; import java.awt.Dimension;
 import java.awt.BorderLayout; import java.awt.GridLayout;
-import java.awt.event.MouseListener; import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener; import java.awt.event.ActionEvent;
-import javax.swing.JFrame; import javax.swing.JPanel;
-import javax.swing.Action; import javax.swing.Box;
-import javax.swing.BoxLayout; import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel; import javax.swing.JButton;
+import javax.swing.JFrame; import javax.swing.JPanel; import javax.swing.Timer;
+import javax.swing.Box; import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel; import javax.swing.JButton; import javax.swing.BoxLayout;
 
 public class TicTacToes extends JFrame {
     int[] scores = new int[3]; //P1 wins, P2 wins, draws
     JLabel[] scoreBoardLabel = new JLabel[3]; //P1 wins, P2 wins, draws
     JLabel winnerLabel = new JLabel("In Progress");
     String nextMove = "X";
+    int filled = 0;
+    boolean inProgress = true;
     TicTacToes() {
         setMinimumSize(new Dimension(480,580));
         setSize(480,580);
@@ -74,6 +71,10 @@ public class TicTacToes extends JFrame {
         newGameButton.addActionListener(new ActionListener() {
             @Override public void actionPerformed(ActionEvent e) {
                 gameCanvas.reset();
+                winnerLabel.setText("In Progress");
+                filled = 0;
+                nextMove = "X";
+                inProgress = true;
             }
         });
         buttonCanvas.setLayout(new GridLayout(1,3));
@@ -108,12 +109,47 @@ public class TicTacToes extends JFrame {
                         getHeight()/2+(int)(5.0/480.0*TicTacToes.this.getWidth()));
                     }
                 };
+                /* Timer
+                Timer timer = new Timer(1000,new ActionListener() {
+                    @Override public void actionPerformed(ActionEvent e) {
+                        GameCanvas.this.reset();
+                        winnerLabel.setText("In Progress");
+                        filled = 0;
+                        ((Timer)e.getSource()).stop();
+                    }
+                }); */
                 labels[i].addActionListener(new ActionListener() {
                     @Override public void actionPerformed(ActionEvent e){
-                        ((JButton)e.getSource()).setText(nextMove);
-                        scores[0]++;
-                        scoreBoardLabel[0].setText("P1 wins: " + scores[0]);
-                        nextMove = (nextMove.equals("X"))? "O" : "X";
+                        JButton current = (JButton)e.getSource();
+                        if (current.getText().isEmpty()&&inProgress == true) {
+                            current.setText(nextMove);
+                            filled++;
+                            if (checkWin(nextMove)){
+                                if (nextMove.equals("X")) {
+                                    scores[0]++;
+                                    winnerLabel.setText("P1 wins");
+                                }
+                                else {
+                                    scores[1]++;
+                                    winnerLabel.setText("P2 wins");
+                                }
+                                inProgress = false;
+                                // timer.start();
+                            }
+                            else {
+                                if (filled == 9) {
+                                    filled = 0;
+                                    scores[2]++;
+                                    winnerLabel.setText("Draws");
+                                    inProgress = false;
+                                    // timer.start();
+                                }
+                            }
+                            scoreBoardLabel[0].setText("P1 wins: " + scores[0]);
+                            scoreBoardLabel[1].setText("P2 wins: " + scores[1]);
+                            scoreBoardLabel[2].setText("Draws: " + scores[2]);
+                            nextMove = (nextMove.equals("X"))? "O" : "X";
+                        }
                     }
                 });
                 labels[i].setHorizontalAlignment(JLabel.CENTER);
@@ -135,6 +171,27 @@ public class TicTacToes extends JFrame {
         public void reset() {
             for (JButton label : labels)
                 label.setText("");
+        }
+        public boolean checkWin(String n) {
+            for (int i = 0; i < 3; i++) {
+                if (labels[i].getText().equals(n) && 
+                    labels[i+3].getText().equals(n) && labels[i+6].getText().equals(n)) {
+                    return true;
+                }
+                if (labels[i*3].getText().equals(n) && 
+                    labels[i*3+1].getText().equals(n) && labels[i*3+2].getText().equals(n)) {
+                    return true;
+                }
+            }
+            if (labels[0].getText().equals(n) && 
+                labels[4].getText().equals(n) && labels[8].getText().equals(n)) {
+                return true;
+            }
+            if (labels[2].getText().equals(n) && 
+                labels[4].getText().equals(n) && labels[6].getText().equals(n)) {
+                return true;
+            }
+            return false;
         }
     }
 }
